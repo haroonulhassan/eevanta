062921d051d8ff7896d28a5ae1ce6b669cd47c45
Haroon Ul Hassan <haroonulhassa1@gmail.com>
2024-01-13 03:36:36 +0500

    first com

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    MONGODB_URI:"mongodb+srv://haroon:3770@cluster2.eeoci4y.mongodb.net/eevanta"
+    MONGODB_URI:"mongodb+srv://haroon:3770@cluster2.eeoci4y.mongodb.net/?retryWrites=true&w=majority"
   }
 }
    
